2328d17dd74d0d83aba49712753d613b37ccabf7
Hélio Coelho <81621491+helinhocoelho@users.noreply.github.com>
2026-04-12 18:18:23 -0300

    Initial commit - Spring Boot syncstock configurado

---
+++ src/main/java/com/sincroniza/syncstock/SyncstockApplication.java
@@ -0,0 +1,13 @@
+package com.sincroniza.syncstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sincroniza/syncstock/SyncstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sincroniza.syncstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
